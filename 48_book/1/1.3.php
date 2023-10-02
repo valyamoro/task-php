1995a7ec173832b4fa5fc26bb12defc846a1a06e
kutlumbek <Moralek228@gmail.com>
2023-10-02 04:35:20 +0300

    Добавил комментарии к 30.09.2023 ДО выполнения

---
+++ 48_book/1/1.3.php
@@ -0,0 +1,34 @@
+<?php
+
+$a = 1;
+
+if ($a === 1) {
+    echo 'monday';
+} elseif ($a === 2) {
+    echo 'Tuesday';
+} elseif ($a === 3) {
+    echo 'Wednesday';
+} elseif ($a === 4) {
+    echo 'Thursday';
+} elseif ($a === 5) {
+    echo 'Friday';
+} elseif ($a === 6) {
+    echo 'Saturday';
+} elseif ($a === 7) {
+    echo 'Sunday';
+} else {
+    echo 'Неверное число';
+}
+
+// -------------------------
+
+$a = 64;
+
+if ($a > 80) {
+    echo 'Здравствуйте уважаемый';
+} else {
+    echo 'успехов';
+}
+// ---------------
+// страны
+// --------------
--- w.php
+++ w.php
@@ -1,2 +1,16 @@
 <?php
 
+function connectionDB(): ?\PDO
+{
+    $dbh = new PDO(
+        'mysql:host=localhost;dbname=db-tt;charset=utf8mb4',
+        'root',
+        '', [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'"
+        ]
+    );
+
+    return $dbh;
+}
