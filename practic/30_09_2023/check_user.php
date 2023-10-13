d66679375b62041e10370d6211d89fccbed74043
kutlumbek <Moralek228@gmail.com>
2023-10-13 18:18:00 +0300

    Мелкий рефакторинг check_user.php

--- practic/30_09_2023/check_user.php
+++ practic/30_09_2023/check_user.php
@@ -1,50 +1,60 @@
 <?php
 
+// Логин пользователя.
 $userLogin = 'r1321e211';
+// Дата входа пользователя на сайт.
 $userLoginDate = date('Y-m-d');
 
+// Массив с данными всех пользователей.
 $dataUsers = file('check_user.txt');
 
+// Получаем текующую дату.
 $currentDate = date('Y-m-d');
 
+// Преобразуем даты в формат timestamp.
 $userLoginDate = strtotime($userLoginDate);
 $currentDate = strtotime($currentDate);
 
-$isUserExists = false;
+// Заходил ли сегодня пользователь.
+$isUserVisited = false;
 
+// Перебираем данные пользователей.
 foreach ($dataUsers as $q) {
-    $userData = explode('|', $q);
-    if ($userData[0] === $userLogin) {
-        $isUserExists = true;
-        break;
-    }
-}
-
-$checkDate = false;
-
-foreach ($dataUsers as $q) {
+    // Разбиваем данные пользователя на логин и дату захода на сайт.
     $user = explode('|', $q);
 
+    // Получаем разницу в секунду между датами.
     $diff = $currentDate - $user[1];
 
+    // Убираем знак минуса.
     $diff = abs($diff);
 
-    $diff_day = intval($diff / (3600 * 24));
+    // Приводим к int и получаем разницу в днях.
+    $diffDay = intval($diff / (3600 * 24));
 
-    if ($diff_day != 1) {
-        $checkDate = true;
+    // Если пользователь заходил сегодня.
+    if ($diffDay !== 0) {
+        // Пользователь сегодня заходил.
+        $isUserVisited = true;
     }
 }
 
-if ($isUserExists && $checkDate) {
+// Проверяем заходил ли сегодня пользователь.
+if ($isUserVisited) {
+    // Выводим информацию.
     echo 'Пользователь уже заходил сегодня';
+    // Завершаем скрипт.
     die;
 }
 
+// Открываем файл, данные будут открыты в двоичном режиме и не будут перезаписаны.
 $handlerDataUser = fopen('check_user.txt', 'a+b');
 
+// Формируем строку с данными пользователя, его логином и датой захода на сайт.
 $userData = "{$userLogin}|{$userLoginDate}";
 
+// Записываем данные пользователя в файл, переводя на другую строку.
 fwrite($handlerDataUser, $userData . PHP_EOL);
+// Закрываем доступ к файлу.
 fclose($handlerDataUser);
 
