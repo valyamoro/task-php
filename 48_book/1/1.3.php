aa80d9873923ff8480034b178e25969c31fd7309
kutlumbek <Moralek228@gmail.com>
2023-11-05 04:59:21 +0300

    48 book

--- 48_book/1/1.3.php
+++ 48_book/1/1.3.php
@@ -1,34 +1,52 @@
 <?php
 
-$a = 1;
-
-if ($a === 1) {
-    echo 'monday';
-} elseif ($a === 2) {
-    echo 'Tuesday';
-} elseif ($a === 3) {
-    echo 'Wednesday';
-} elseif ($a === 4) {
-    echo 'Thursday';
-} elseif ($a === 5) {
-    echo 'Friday';
-} elseif ($a === 6) {
-    echo 'Saturday';
-} elseif ($a === 7) {
-    echo 'Sunday';
-} else {
-    echo 'Неверное число';
-}
-
-// -------------------------
-
-$a = 64;
-
-if ($a > 80) {
-    echo 'Здравствуйте уважаемый';
-} else {
-    echo 'успехов';
-}
+//$a = 1;
+//
+//if ($a === 1) {
+//    echo 'monday';
+//} elseif ($a === 2) {
+//    echo 'Tuesday';
+//} elseif ($a === 3) {
+//    echo 'Wednesday';
+//} elseif ($a === 4) {
+//    echo 'Thursday';
+//} elseif ($a === 5) {
+//    echo 'Friday';
+//} elseif ($a === 6) {
+//    echo 'Saturday';
+//} elseif ($a === 7) {
+//    echo 'Sunday';
+//} else {
+//    echo 'Неверное число';
+//}
+//
+//// -------------------------
+//
+//$a = 64;
+//
+//if ($a > 80) {
+//    echo 'Здравствуйте уважаемый';
+//} else {
+//    echo 'успехов';
+//}
 // ---------------
 // страны
 // --------------
+?>
+<form>
+    <input name="country" value="egypet" type="checkbox">
+    <label for="html">Египет</label>
+    <input name="country" value="italia" type="checkbox">
+    <label for="html">Италия</label>
+    <input name="country" value="turcia" type="checkbox">
+    <label for="html">Греция</label>
+    <button type="submit" class="btn btn-primary">Выбрать страну</button>
+</form>
+    <form>
+        <input name="restDays" value="w" type="text">
+        <label for="html">Египет</label>
+        <button type="submit" class="btn btn-primary">Выбрать страну</button>
+    </form>
+<?php
+
+print_r($_REQUEST);
--- practic/29_10_2023/audit.php
+++ practic/29_10_2023/audit.php
@@ -3,81 +3,134 @@ declare(strict_types=1);
 // Время выполнения: 2 часа 54 минуты.
 error_reporting(-1);
 
-// Папка с файлами аудитов.
-const DIRECTORY_AUDIT = 'audit/';
-
-// Данные пользователя.
-$user = [
-    'username' => 'ivan',
-    'date' => date('d-m-Y'),
-];
-
-/** Получаю последний созданный файл в директории.
- * @param string $directory
- * @return string|null
- */
-function getLastCreatedFile(string $directory): ?string
+function getFiles(string $folder): ?string
 {
-    // Получаю массив всех файлов, переворачивая его.
-    $result = \scandir($directory, SCANDIR_SORT_DESCENDING);
-    // Удаляю точки.
-    $result = \array_diff($result, ['..', '.']);
-    // Возвращаю последний созданный файл.
-    return $result[0];
+    $files = \scandir($folder, SCANDIR_SORT_ASCENDING);
+    $files = \array_diff($files, ['..', '.']);
+
+    return $files[0] ?? null;
 }
 
-/** Запись данных в файл.
- * @param string $file
- * @param string $data
- * @return void
- */
-function writeFile(string $file, string $data): void
+function writeAllText(string $content, string $filePath, string $mode = 'a+'): bool
 {
-    // Открываю файл на запись. Если не существует - создаю.
-    $handler = \fopen($file, 'a');
-    // Записываю в файл данные.
-    \fwrite($handler, $data . "\n");
+    $handler = \fopen($filePath, $mode);
+
+    return (bool)\fwrite($handler, $content . "\n");
 }
 
-// Привожу данные к нужному формату.
-$user = \implode(';', $user);
+function auditManager(string $visitorName, string $timeOfDate, string $folder): ?string
+{
+    $content = $visitorName . ';' . $timeOfDate;
 
-// Получаю последний созданный файл.
-$currentFile = getLastCreatedFile(DIRECTORY_AUDIT);
+    $lastFile = getFiles($folder);
 
-// Полный путь до файла с записями аудита.
-$fileAudit = null;
+    if (is_null($lastFile)) {
+        $newFile = $folder . '/audit_1.txt';
+        return writeAllText($content, $newFile) ? $content : null;
+    }
 
-// Если папка, хранящая аудиты пуста:
-if (\is_null($currentFile)) {
-    // Путь до первого файла с записями.
-    $fileAudit = DIRECTORY_AUDIT . '/audit_1.txt';
-    // Создаю новый файл и записываю туда пользователя.
-    writeFile($fileAudit, $user);
-} else {
-    // true, если строк меньше трех, иначе false.
-    $isWriteFile = \count(\file(DIRECTORY_AUDIT . $currentFile)) < 3;
+    $filePath = $folder . '/' . $lastFile;
+    $isWriteFile = \count(\file($filePath)) < 3;
 
     if ($isWriteFile === true) {
-        // Путь до последнего файла.
-        $fileAudit = DIRECTORY_AUDIT . $currentFile;
-        // Записываю в последний файл данные пользователя.
-        writeFile($fileAudit, $user);
-    } else {
-        // Получаю индекс аудита.
-        preg_match("/\d+/", $currentFile, $matches);
-        // Помещаю индекс аудита в переменную.
-        $currentFileIndex = $matches[0];
-        // Увеличиваю на единицу индекс аудита.
-        $currentFileIndex++;
-        // Формирую путь до нового файла с аудитом.
-        $fileAudit = DIRECTORY_AUDIT . "audit_{$currentFileIndex}.txt";
-        // Создаю файл с аудитом и записываю туда пользователя.
-        writeFile($fileAudit, $user);
+        return writeAllText($content, $filePath, 'a+') ? $content : null;
     }
+
+    preg_match("/\d+/", $lastFile, $matches);
+    $currentFileIndex = $matches[0] + 1;
+    $filePath = $folder . '/' . "audit_{$currentFileIndex}.txt";
+    return writeAllText($content, $filePath, 'a+') ? $content : null;
+
+
 }
 
-// Если переменная null, то произошла ошибка.
-if (is_null($fileAudit)) {
-    echo 'Что-то пошло не так';
-}
+/////////////////////////////
+
+$userName = 'Ivan';
+$timeOfDate = \date('d-m-Y H:i:s');
+$folder = __DIR__ . '/audit';
+
+$result = auditManager($userName, $timeOfDate, $folder);
+
+print_r($result);
+
+
+
+// Папка с файлами аудитов.
+//const DIRECTORY_AUDIT = 'audit';
+//
+//// Данные пользователя.
+//$user = [
+//    'username' => 'ivan',
+//    'date' => date('d-m-Y'),
+//];
+//
+///** Получаю последний созданный файл в директории.
+// * @param string $directory
+// * @return string|null
+// */
+//function getLastCreatedFile(string $directory): ?string
+//{
+//    // Получаю массив всех файлов, переворачивая его.
+//    $result = \scandir($directory, SCANDIR_SORT_DESCENDING);
+//    // Удаляю точки.
+//    $result = \array_diff($result, ['..', '.']);
+//    // Возвращаю последний созданный файл.
+//    return $result[0];
+//}
+//
+///** Запись данных в файл.
+// * @param string $file
+// * @param string $data
+// * @return void
+// */
+//function writeFile(string $file, string $data): void
+//{
+//    // Открываю файл на запись. Если не существует - создаю.
+//    $handler = \fopen($file, 'a');
+//    // Записываю в файл данные.
+//    \fwrite($handler, $data . "\n");
+//}
+//
+//// Привожу данные к нужному формату.
+//$user = \implode(';', $user);
+//
+//// Получаю последний созданный файл.
+//$currentFile = getLastCreatedFile(DIRECTORY_AUDIT);
+//
+//// Полный путь до файла с записями аудита.
+//$fileAudit = null;
+//
+//// Если папка, хранящая аудиты пуста:
+//if (\is_null($currentFile)) {
+//    // Путь до первого файла с записями.
+//    $fileAudit = DIRECTORY_AUDIT . '/audit_1.txt';
+//    // Создаю новый файл и записываю туда пользователя.
+//    writeFile($fileAudit, $user);
+//} else {
+//    // true, если строк меньше трех, иначе false.
+//    $isWriteFile = \count(\file(DIRECTORY_AUDIT . $currentFile)) < 3;
+//
+//    if ($isWriteFile === true) {
+//        // Путь до последнего файла.
+//        $fileAudit = DIRECTORY_AUDIT . $currentFile;
+//        // Записываю в последний файл данные пользователя.
+//        writeFile($fileAudit, $user);
+//    } else {
+//        // Получаю индекс аудита.
+//        preg_match("/\d+/", $currentFile, $matches);
+//        // Помещаю индекс аудита в переменную.
+//        $currentFileIndex = $matches[0];
+//        // Увеличиваю на единицу индекс аудита.
+//        $currentFileIndex++;
+//        // Формирую путь до нового файла с аудитом.
+//        $fileAudit = DIRECTORY_AUDIT . "/audit_{$currentFileIndex}.txt";
+//        // Создаю файл с аудитом и записываю туда пользователя.
+//        writeFile($fileAudit, $user);
+//    }
+//}
+//
+//// Если переменная null, то произошла ошибка.
+//if (is_null($fileAudit)) {
+//    echo 'Что-то пошло не так';
+//}
